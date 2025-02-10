45d89a13a404035fba390dd96056ac500cb94cad
João Canabarro Campos <joaocampos963@icloud.com>
2025-02-10 11:52:46 -0300

    Applied general styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div>
       <nav>
-        <div>
+        <div className="container-calculator">
           <Link className="title" to="/">
             Calculator
           </Link>
